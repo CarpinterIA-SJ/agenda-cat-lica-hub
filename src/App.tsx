b0860c16e4a9ed7ca01534522fc0d5853a1317b7
CarpinterIA-SJ <carpinteria.ia.sj@gmail.com>
2026-03-26 10:03:58 -0300

    Remover acesso à rota de dashboard do organizador

--- src/App.tsx
+++ src/App.tsx
@@ -63,6 +63,7 @@ const App = () => (
               <Route path="/support" element={<SupportPage />} />
             </Route>
 
+            <Route path="/organizador/dashboard" element={<Navigate to="/crm" replace />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
